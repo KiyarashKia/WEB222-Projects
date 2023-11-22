742d5ed00a0d40af19c04d632046a9cb3a3a2664
Kiyarash Kia <kiarashkia1999@gmail.com>
2023-11-22 04:14:44 -0500

    Main stage 1/2

--- Assignment 5/js/app.js
+++ Assignment 5/js/app.js
@@ -10,115 +10,102 @@
  *
  *      Name:       Kiarash Kia
  *      Student ID: 108688235
- *      Date:       11/14/2023
+ *      Date:       11/21/2023
  */
 
 // All of our data is available on the global `window` object.
 // Create local variables to work with it in this file.
 const { artists, songs } = window;
 
-// For debugging, display all of our data in the console. You can remove this later.
-console.log({ artists, songs }, "App Data");
+function createSongCard(song) {
+  const card = document.createElement("div");
+  card.className = "songs-card";
 
-const tableHeaders = ["Song", "Year", "Length", "Play"];
+  // Add song cover image
+  const coverImg = document.createElement("img");
+  coverImg.src = song.coverImageUrl || 'default-image-url'; // Add a default image URL if needed
+  coverImg.alt = `Cover of ${song.title}`;
+  card.appendChild(coverImg);
 
-tableHeaders.forEach((header) => {
-  const thead = document.getElementById("header");
-  const th = document.createElement("th");
-  th.textContent = header;
-  thead.appendChild(th);
-  th.className = "tabletitle";
-});
+  // Song Title
+  const title = document.createElement("h2");
+  title.textContent = song.title;
+  card.appendChild(title);
+
+    // Line Break
+    card.appendChild(document.createElement("br"));
+  
+  // Length
+  const length = document.createElement("span");
+  length.innerHTML = `<b>Duration:</b> ${formatDuration(song.duration)}`;
+  card.appendChild(length);
+  
+
+ // Year
+ const year = document.createElement("time");
+ year.textContent = `Year Recorded: ${song.year}`;
+ card.appendChild(year);
+
+  // Line Break
+  card.appendChild(document.createElement("br"));
+  card.appendChild(document.createElement("br"));
+
+  return card;
+}
+
 
 function listSongs(artistId) {
-  const tbody = document.getElementById("songs");
+  const songsContainer = document.querySelector(".card-container"); // Use class selector
+  songsContainer.innerHTML = ''; // Clear existing content
 
   let songsByArtist = songs.filter((song) => song.artistId === artistId);
   songsByArtist.forEach((song) => {
-    const tr = document.createElement("tr");
-
-    // Song Title
-    const tdSong = document.createElement("td");
-    tdSong.textContent = song.title;
-    tr.appendChild(tdSong);
-
-    // Year
-    const tdYear = document.createElement("td");
-    tdYear.textContent = song.year;
-    tr.appendChild(tdYear);
-
-    // Length
-    const tdLength = document.createElement("td");
-    tdLength.textContent = formatDuration(song.duration);
-    tr.appendChild(tdLength);
-
-    // Play Button
-    const tdButton = document.createElement("td");
-    const playButton = document.createElement("button");
-    playButton.textContent = "Play";
-    playButton.className = "play-button"; // I Added a class to stylize the button
-    playButton.addEventListener("click", () => window.open(song.url, "_blank"));
-    tdButton.appendChild(playButton);
-    tr.appendChild(tdButton);
-
-    tbody.appendChild(tr);
+    const songCard = createSongCard(song);
+    songsContainer.appendChild(songCard);
   });
 }
 
 function listArtists(artists) {
-  console.log("listing artists");
+  // Assuming there's a menu element where artist buttons will be added
   const menu = document.getElementById("menu");
-  const buttonMenu = document.createElement("div");
-  buttonMenu.className = "buttonMenu";
-  menu.appendChild(buttonMenu);
-  const urlsDiv = document.createElement("div");
-  urlsDiv.id = "artistUrls";
+  menu.innerHTML = '';
 
-  artists.forEach((artist) => {
+  artists.forEach(artist => {
     const artistButton = document.createElement("button");
     artistButton.className = "button";
     artistButton.textContent = artist.name;
-    buttonMenu.appendChild(artistButton);
-
     artistButton.addEventListener("click", () => {
-      const title = document.getElementById("artists");
-      title.textContent = artist.name;
-
-      const existingUrlsDiv = document.getElementById("artistUrls");
-      if (existingUrlsDiv) {
-        existingUrlsDiv.remove();
-      }
-
-      const newUrlsDiv = document.createElement("div");
-      newUrlsDiv.id = "artistUrls";
-
-      title.parentNode.appendChild(newUrlsDiv);
-
-      artist.urls.forEach((url) => {
-        const urlElement = document.createElement("a");
-        urlElement.href = url.url;
-        urlElement.textContent = url.name;
-        urlElement.target = "_blank";
-        newUrlsDiv.appendChild(urlElement);
-        newUrlsDiv.appendChild(document.createElement("br"));
-      });
-
-      const tbody = document.getElementById("songs");
-      tbody.remove();
-      const tbodyReset = document.createElement("tbody");
-      tbodyReset.id = "songs";
-      const table = document.querySelector("table");
-      table.appendChild(tbodyReset);
+      displayArtistInfo(artist.artistId);
       listSongs(artist.artistId);
     });
+    menu.appendChild(artistButton);
   });
 }
 
+function displayArtistInfo(artistId) {
+  // Adjusted to use only one artist-info section
+  const artistInfo = document.getElementById("artist-info");
+  artistInfo.innerHTML = '';
+
+  const artist = artists.find(a => a.artistId === artistId);
+  if (artist) {
+    const nameElement = document.createElement("h2");
+    nameElement.textContent = artist.name;
+    artistInfo.appendChild(nameElement);
+
+    artist.urls.forEach(url => {
+      const urlElement = document.createElement("a");
+      urlElement.href = url.url;
+      urlElement.textContent = url.name;
+      urlElement.target = "_blank";
+      artistInfo.appendChild(urlElement);
+      artistInfo.appendChild(document.createElement("br"));
+    });
+  }
+}
 function onLoad() {
-  console.log("loaded");
   listArtists(artists);
 
-  // Check if there is at least one artist.based on the instrction
   if (artists.length > 0) {
     const firstArtistId = artists[0].artistId;
     displayArtistInfo(firstArtistId);
@@ -126,35 +113,10 @@ function onLoad() {
   }
 }
 
-window.addEventListener("load", onLoad);
-
-function displayArtistInfo(artistId) {
-  const artist = artists.find((a) => a.artistId === artistId);
-  if (artist) {
-    const title = document.getElementById("artists");
-    title.textContent = artist.name;
-
-    const urlsDiv = document.getElementById("artistUrls") || document.createElement("div");
-    urlsDiv.id = "artistUrls";
-    urlsDiv.innerHTML = "";
-
-    artist.urls.forEach((url) => {
-      const urlElement = document.createElement("a");
-      urlElement.href = url.url;
-      urlElement.textContent = url.name;
-      urlElement.target = "_blank";
-      urlsDiv.appendChild(urlElement);
-      urlsDiv.appendChild(document.createElement("br"));
-    });
-
-    title.parentNode.appendChild(urlsDiv);
-  }
-}
-
 function formatDuration(duration) {
-  const minutes = parseInt(duration / 60, 10);
+  const minutes = Math.floor(duration / 60);
   const seconds = duration % 60;
-  return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+  return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 }
 
 window.addEventListener("load", onLoad);
